c9913f838becbe7662bdd3e1e8baab5b5a62c41a
Rakshanda <rakshanda92041@gmail.com>
2019-11-21 00:41:56 -0800

    Initial commit

---
+++ app/src/main/java/com/example/fused/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.fused;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends FragmentActivity implements
+        OnMapReadyCallback{
+    Location mlocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int request_code=101;
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+GetlastLocation();
+}
+
+    private void GetlastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, request_code);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    mlocation = location;
+                    Toast.makeText(getApplicationContext(), mlocation.getAltitude() + "" + mlocation.getLongitude(),
+                            Toast.LENGTH_SHORT).show();
+
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+                    supportMapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //used to show langitude on map
+        LatLng latLng=new LatLng(mlocation.getLatitude(),mlocation.getLongitude());
+        //use to show marker
+        MarkerOptions markerOptions=new MarkerOptions().position(latLng).title("You are here");
+        //by making animate factory
+        // used to zoom camera on your location
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 6));
+        googleMap.addMarker(markerOptions);
+
+        // Add a marker in Sydney and move the camera
+        mMap.setMyLocationEnabled(true);
+
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case request_code:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    GetlastLocation();
+                }
+                break;
+        }
+    }
+}
